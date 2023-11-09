c2bfec9acc6829f0613c802450104ef8460dab40
TrueBRS <707291789@qq.com>
2023-11-09 13:32:03 +0800

    Initial commit
    WaterFiltrationPRO 1.0.0

---
+++ WaterFiltrationPRO/FiltrationMachinePatch.cs
@@ -0,0 +1,100 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace WaterFiltrationPRO
+{
+	[HarmonyPatch(typeof(FiltrationMachine))]
+	public class FiltrationMachinePatch
+	{
+		// Show the seconds when hovering
+		[HarmonyPatch(nameof(FiltrationMachine.OnHover))]
+		[HarmonyPostfix]
+		public static void OnHoverPatch(FiltrationMachine __instance)
+		{
+			if (Plugin.ModOptions.hoverSecEnable)
+			{
+				// Basically vanilla conditions
+				if (!(__instance.constructed < 1f) && !(GameModeUtils.RequiresPower() && __instance.powerRelay.GetPower() < 0.85f) && (__instance.timeRemainingWater >= 0f || __instance.timeRemainingSalt >= 0f))
+				{
+					string myText = Language.main.Get("water") + ": " + $"{Mathf.Ceil(__instance.timeRemainingWater)}" + Language.main.Get("s") + ", " + Language.main.Get("salt") + ": " + $"{Mathf.Ceil(__instance.timeRemainingSalt)}" + Language.main.Get("s");
+					HandReticle.main.SetText(HandReticle.TextType.HandSubscript, myText, false, GameInput.Button.None);
+				}
+			}
+		}
+
+		// Consume more power based on FilteringScaleSliderValue, in total will consume vanilla + 0.85 * FilteringMultiplierSliderValue every second
+		[HarmonyPatch(nameof(FiltrationMachine.UpdateFiltering))]
+		[HarmonyPrefix]
+		public static void UpdateFilteringPatch(FiltrationMachine __instance)
+		{
+			if (__instance._constructed < 1f)
+			{
+				return;
+			}
+			if (Plugin.ModOptions.FilteringMultiplierPowerEnable && Plugin.ModOptions.FilteringMultiplierEnable)
+			{
+				float num = 1f * DayNightCycle.main.dayNightSpeed;
+				if ((__instance.timeRemainingWater > 0f || __instance.timeRemainingSalt > 0f) && (__instance.powerRelay != null && __instance.powerRelay.GetPower() >= 0.85f * num) && GameModeUtils.RequiresPower())
+				{
+					float num2;
+					__instance.powerRelay.ConsumeEnergy((0.85f * Plugin.ModOptions.FilteringMultiplierSliderValue) * num, out num2);
+				}
+			}
+		}
+
+		// set the timeRemainingWater
+		[HarmonyPatch(nameof(FiltrationMachine.TryFilterWater))]
+		[HarmonyPrefix]
+		public static void TryFilterWaterPatch(FiltrationMachine __instance)
+		{
+			if (__instance.timeRemainingWater > 0f)
+			{
+				return;
+			}
+			if (!__instance.IsUnderwater() && __instance.atmosphericWaterScalar == 0f)
+			{
+				return;
+			}
+			// This should takeover the vanilla code
+			if (__instance.storageContainer.container.GetCount(TechType.BigFilteredWater) < (Plugin.ModOptions.MaxWaterSliderValue))
+			{
+				if (Plugin.ModOptions.FilteringMultiplierEnable)
+				{
+					__instance.timeRemainingWater = 840f / Plugin.ModOptions.FilteringMultiplierSliderValue;
+				}
+				else
+				{
+					__instance.timeRemainingWater = 840f;
+				}
+				return;
+			}
+		}
+
+		// set the timeRemainingSalt
+		[HarmonyPatch(nameof(FiltrationMachine.TryFilterSalt))]
+		[HarmonyPrefix]
+		public static void TryFilterSaltPatch(FiltrationMachine __instance)
+		{
+			if (__instance.timeRemainingSalt > 0f)
+			{
+				return;
+			}
+			if (!__instance.IsUnderwater() && __instance.atmosphericSaltScalar == 0f)
+			{
+				return;
+			}
+			if (__instance.storageContainer.container.GetCount(TechType.Salt) < Plugin.ModOptions.MaxSaltSliderValue)
+			{
+				if (Plugin.ModOptions.FilteringMultiplierEnable)
+				{
+					__instance.timeRemainingSalt = 420f / Plugin.ModOptions.FilteringMultiplierSliderValue;
+				}
+				else
+				{
+					__instance.timeRemainingSalt = 420f;
+				}
+				return;
+			}
+		}
+	}
+}
---
+++ WaterFiltrationPRO/ModOptions.cs
@@ -0,0 +1,56 @@
+﻿using Nautilus.Json;
+using Nautilus.Options;
+using Nautilus.Options.Attributes;
+
+namespace WaterFiltrationPRO
+{
+	[Menu("WaterFiltrationPRO")]
+	public class ModOptions : ConfigFile
+	{
+		[Toggle(LabelLanguageId = "hoverSecToggleLabel", TooltipLanguageId = "hoverSecToggleTT"), OnChange(nameof(HoverSecToggleEvent))]
+		public bool hoverSecEnable = true;
+
+		[Toggle(LabelLanguageId = "FilteringMultiplierToggleLabel", TooltipLanguageId = "FilteringMultiplierToggleTT"), OnChange(nameof(FilteringMultiplierToggleEvent))]
+		public bool FilteringMultiplierEnable = false;
+
+		[Toggle(LabelLanguageId = "FilteringMultiplierPowerToggleLabel", TooltipLanguageId = "FilteringMultiplierPowerToggleTT"), OnChange(nameof(FilteringMultiplierPowerToggleEvent))]
+		public bool FilteringMultiplierPowerEnable = true;
+
+		[Slider(LabelLanguageId = "FilteringMultiplierSliderLabel", TooltipLanguageId = "FilteringMultiplierSliderTT", Format = "{0:F0}", Max = 5f, Min = 1f, DefaultValue = 1f, Step = 1F), OnChange(nameof(FilteringMultiplierChangedEvent))]
+		public float FilteringMultiplierSliderValue;
+
+		[Slider(LabelLanguageId = "MaxWaterSliderLabel", TooltipLanguageId = "MaxWaterSliderTT", Format = "{0:F0}", Max = 10f, Min = 2f, DefaultValue = 2f, Step = 1F), OnChange(nameof(MaxWaterChangedEvent))]
+		public float MaxWaterSliderValue;
+
+		[Slider(LabelLanguageId = "MaxSaltSliderLabel", TooltipLanguageId = "MaxSaltSliderTT", Format = "{0:F0}", Max = 10f, Min = 2f, DefaultValue = 2f, Step = 1F), OnChange(nameof(MaxSaltChangedEvent))]
+		public float MaxSaltSliderValue;
+
+		private void HoverSecToggleEvent(ToggleChangedEventArgs e)
+		{
+			bool booleanValue = e.Value;
+		}
+
+		private void FilteringMultiplierToggleEvent(ToggleChangedEventArgs e)
+		{
+			bool booleanValue = e.Value;
+		}
+
+		private void FilteringMultiplierChangedEvent(SliderChangedEventArgs e)
+		{
+			float floatValue = e.Value;
+		}
+
+		private void FilteringMultiplierPowerToggleEvent(ToggleChangedEventArgs e)
+		{
+			bool booleanValue = e.Value;
+		}
+		private void MaxWaterChangedEvent(SliderChangedEventArgs e)
+		{
+			float floatValue = e.Value;
+		}
+		private void MaxSaltChangedEvent(SliderChangedEventArgs e)
+		{
+			float floatValue = e.Value;
+		}
+	}
+}
