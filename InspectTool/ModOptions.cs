b32a126f16e8877a6bfeb950ec48b72fab038a8d
TrueBRS <707291789@qq.com>
2023-11-15 11:18:51 +0800

    InspectTool 1.0.0
    All credit goes to MrPurple6411.

---
+++ InspectTool/ModOptions.cs
@@ -0,0 +1,19 @@
+﻿using Nautilus.Json;
+using Nautilus.Options;
+using Nautilus.Options.Attributes;
+using UnityEngine;
+
+namespace InspectTool
+{
+	[Menu("InspectTool")]
+	public class ModOptions : ConfigFile
+	{
+		[Keybind(LabelLanguageId = "masterKeybindLabel", TooltipLanguageId = "masterKeybindTT"), OnChange(nameof(masterKeybindValueChangedEvent))]
+		public KeyCode masterKeybindKey = KeyCode.I;
+
+		private void masterKeybindValueChangedEvent(KeybindChangedEventArgs e)
+		{
+			KeyCode keyCode = e.Value;
+		}
+	}
+}
--- InspectTool/Plugin.cs
+++ InspectTool/Plugin.cs
@@ -4,24 +4,21 @@ using HarmonyLib;
 using Nautilus.Handlers;
 using System.Reflection;
 
-namespace WaterFiltrationPRO
+namespace InspectTool
 {
 	[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 	[BepInDependency("com.snmodding.nautilus")]
 	public class Plugin : BaseUnityPlugin
 	{
 		public new static ManualLogSource Logger { get; private set; }
-
 		private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
 
-		// declare static ModOptions for use elsewhere
 		public static ModOptions ModOptions;
 
 		private void Awake()
 		{
 			LanguageHandler.RegisterLocalizationFolder();
 
-			// Set up Mod Options
 			ModOptions = OptionsPanelHandler.RegisterModOptions<ModOptions>();
 
 			// set project-scoped logger instance
