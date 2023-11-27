025e4353e2afab4408859765f93cbf97d94a3a3e
TrueBRS <707291789@qq.com>
2023-11-27 09:02:35 +0800

    Updated 2 old mods from others
    1. Eye_Friendly_Flare = Save_My_Eyes__Flares_edition by kelsarhu
    2. Feedback_Reminder_Be_Gone = No Feedback Reminder by yw2theorycrafter and kairality

---
+++ Eye_Friendly_Flare/FeedbackCollector_Awake_Patch.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Eye_Friendly_Flare
+{
+	[HarmonyPatch(typeof(Flare))]
+	[HarmonyPatch("UpdateLight")]
+	internal class Flare_intensity_Patch
+	{
+		[HarmonyPostfix]
+		public static void Postfix(Flare __instance)
+		{
+			float num = 2f;
+			bool flag = __instance.energyLeft > 1790f;
+			if (flag)
+			{
+				__instance.light.intensity = Mathf.Lerp(0.2f, num, (1790f - __instance.energyLeft + 10f) / 10f);
+			}
+			else
+			{
+				bool flag2 = __instance.energyLeft < 1000f;
+				if (flag2)
+				{
+					__instance.light.intensity = Mathf.Lerp(1f, num, __instance.energyLeft / 1000f);
+				}
+				else
+				{
+					__instance.light.intensity = num;
+				}
+			}
+		}
+	}
+}
---
+++ Feedback Reminder Be Gone/FeedbackCollector_Awake_Patch.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using System.Reflection;
+
+namespace Feedback_Reminder_Be_Gone.Patches
+{
+	[HarmonyPatch]
+	public class FeedbackCollector_Awake_Patch
+	{
+#pragma warning disable IDE0051 // Remove unused private members
+		static MethodBase TargetMethod()
+		{
+			//Awake is a protected method, so we have to set the BindingFlags accordingly.
+			return typeof(uGUI_FeedbackCollector).GetMethod(
+				"Awake",
+				BindingFlags.Instance | BindingFlags.NonPublic);
+		}
+#pragma warning disable IDE0051 // Remove unused private members
+		static void Postfix(MethodBase __originalMethod, uGUI_FeedbackCollector __instance)
+		{
+#if DEBUG
+			Plugin.Logger.LogInfo("Patching GUI element " + __instance.ToString() + " with " + __originalMethod.ToString());
+#endif
+			var couldFind = false;
+			foreach (UnityEngine.Transform child in __instance.transform)
+			{
+				if (child.name.Equals("FeedbackHint"))
+				{
+					child.gameObject.SetActive(false);
+					couldFind = true;
+#if DEBUG
+					Plugin.Logger.LogInfo("Successfully disabled " + child.ToString() + " of GUI element " + __instance.ToString());
+#endif
+					goto searchEnd;
+				}
+			}
+		searchEnd: if (!couldFind)
+			{
+				Plugin.Logger.LogInfo("WARN: Couldn't find FeedbackHint child of GUI element " + __instance.ToString());
+			}
+		}
+	}
+}
