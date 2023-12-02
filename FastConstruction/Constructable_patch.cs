c6bdb8c366d9d9d964d27d468d62c2e2210e5d62
TrueBRS <707291789@qq.com>
2023-12-02 09:11:55 +0800

    Updated FastConstruction
    FastConstruction = Fast Build by Remodor

---
+++ FastConstruction/Constructable_patch.cs
@@ -0,0 +1,59 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace FastConstruction
+{
+	//internal static class Plugin.ModOptions
+	//{
+	//	//internal static float buildingSpeed = 1f;
+	//	//internal static float maximumBuildTime = 6f;
+	//	//internal static float minimumBuildTime = 2f;
+	//	//internal static bool progressive = true;
+	//}
+	[HarmonyPatch(typeof(Constructable))]
+	[HarmonyPatch(nameof(Constructable.GetConstructInterval))]
+	internal class Constructable_GetConstructInterval_Patch
+	{
+		internal static int numberOfResources = 1;
+		private static float calculateLogisticBuildingSpeed()
+		{
+			float buildDelta = Plugin.ModOptions.maximumBuildTime - Plugin.ModOptions.minimumBuildTime;
+			float offset = buildDelta - Plugin.ModOptions.minimumBuildTime;
+			return ((buildDelta * 2) / (1 + Mathf.Exp(-1 * Plugin.ModOptions.buildingSpeed * (numberOfResources - 1))) - offset);
+		}
+		static void Postfix(ref float __result)
+		{
+			if (__result == 1)
+			{
+				if (!Plugin.ModOptions.progressiveEnable)
+				{
+					float minimumBuildSpeed = Plugin.ModOptions.minimumBuildTime / numberOfResources;
+					float maximumBuildSpeed = Plugin.ModOptions.maximumBuildTime / numberOfResources;
+					__result = Mathf.Clamp(Plugin.ModOptions.buildingSpeed, minimumBuildSpeed, maximumBuildSpeed);
+				}
+				else
+				{
+					__result = Mathf.Max(calculateLogisticBuildingSpeed(), 0.1f) / numberOfResources;
+				}
+			}
+		}
+	}
+	[HarmonyPatch(typeof(Constructable))]
+	[HarmonyPatch(nameof(Constructable.Construct))]
+	internal class Constructable_Construct_Patch
+	{
+		static void Prefix(Constructable __instance)
+		{
+			Constructable_GetConstructInterval_Patch.numberOfResources = __instance.resourceMap.Count;
+		}
+	}
+	[HarmonyPatch(typeof(Constructable))]
+	[HarmonyPatch(nameof(Constructable.DeconstructAsync))]
+	internal class Constructable_Deconstruct_Patch
+	{
+		static void Prefix(Constructable __instance)
+		{
+			Constructable_GetConstructInterval_Patch.numberOfResources = __instance.resourceMap.Count;
+		}
+	}
+}
---
+++ FastConstruction/ModOptions.cs
@@ -0,0 +1,38 @@
+﻿using Nautilus.Json;
+using Nautilus.Options;
+using Nautilus.Options.Attributes;
+
+namespace FastConstruction
+{
+	[Menu("FastConstruction")]
+	public class ModOptions : ConfigFile
+	{
+		[Toggle(Label = "Progressive", Tooltip = "*Disable = \"Linear\": The building speed is the time per used item. if you set it to 1 and the current building requires 2 resources, the building time will be 2, except if you set a lower maximum time or a higher minimum time.\n\n*Enable = \"Progressive\": The added time per used resource decreases the more resources are used. It uses the Logistic Function﻿. The maximum is the converging value. The minimum is the lowest value."), OnChange(nameof(ProgressiveToggleEvent))]
+		public bool progressiveEnable = false;
+		private void ProgressiveToggleEvent(ToggleChangedEventArgs e)
+		{
+			bool booleanValue = e.Value;
+		}
+
+		[Slider(Label = "Building Speed", Tooltip = "When the progressive mode is selected, this value is the \"growth rate\". Otherwise it is the time per used resource.", Format = "{0:F1}", Min = 0.1f, Max = 2f, DefaultValue = 1f, Step = 0.1F), OnChange(nameof(BuildingSpeedChangedEvent))]
+		public float buildingSpeed = 1f;
+		private void BuildingSpeedChangedEvent(SliderChangedEventArgs e)
+		{
+			float floatValue = e.Value;
+		}
+
+		[Slider(Label = "Maximum Build Time", Tooltip = "The total construction time will never surpass this value.", Format = "{0:F1}", Min = 0.1f, Max = 10f, DefaultValue = 10f, Step = 0.1F), OnChange(nameof(maximumBuildTimeChangedEvent))]
+		public float maximumBuildTime = 10f;
+		private void maximumBuildTimeChangedEvent(SliderChangedEventArgs e)
+		{
+			float floatValue = e.Value;
+		}
+
+		[Slider(Label = "Minimum Build Time", Tooltip = "The construction time will never fall below this value.", Format = "{0:F1}", Min = 0.1f, Max = 10f, DefaultValue = 1f, Step = 0.1F), OnChange(nameof(minimumBuildTimeChangedEvent))]
+		public float minimumBuildTime = 1f;
+		private void minimumBuildTimeChangedEvent(SliderChangedEventArgs e)
+		{
+			float floatValue = e.Value;
+		}
+	}
+}
