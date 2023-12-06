d815dfb6f2a1eeda5d20cf096dcd291caf9199e3
TrueBRS <707291789@qq.com>
2023-12-06 12:26:13 +0800

    VoiceNotificationInterval 1.0.0

---
+++ VoiceNotificationInterval/ModOptions.cs
@@ -0,0 +1,24 @@
+﻿using Nautilus.Json;
+using Nautilus.Options;
+using Nautilus.Options.Attributes;
+
+namespace VoiceNotificationInterval
+{
+	[Menu("Voice Notification Interval")]
+	public class ModOptions : ConfigFile
+	{
+		[Toggle(LabelLanguageId = "masterEnableLabel", TooltipLanguageId = "masterEnableTT"), OnChange(nameof(MasterEnableToggleEvent))]
+		public bool masterEnable = false;
+		private void MasterEnableToggleEvent(ToggleChangedEventArgs e)
+		{
+			bool booleanValue = e.Value;
+		}
+
+		[Slider(LabelLanguageId = "minIntervalLabel", TooltipLanguageId = "minIntervalTT", Format = "{0:F0}", Min = 1f, Max = 60f, DefaultValue = 15f, Step = 1F), OnChange(nameof(minIntervalChangedEvent))]
+		public float minInterval = 21f;
+		private void minIntervalChangedEvent(SliderChangedEventArgs e)
+		{
+			float floatValue = e.Value;
+		}
+	}
+}
---
+++ VoiceNotificationInterval/VoiceNotification_Play.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System;
+
+namespace VoiceNotificationInterval
+{
+	[HarmonyPatch(typeof(VoiceNotification), nameof(VoiceNotification.Play), new Type[] { typeof(object[]) })]
+	internal class VoiceNotification_Play
+	{
+		[HarmonyPostfix]
+		private static void Postfix(VoiceNotification __instance)
+		{
+			try
+			{
+				if (Plugin.ModOptions.masterEnable)
+				{
+					__instance.timeNextPlay = DayNightCycle.main.timePassedAsFloat + Plugin.ModOptions.minInterval;
+					//Plugin.Logger.LogInfo($"DayNightCycle.main.timePassedAsFloat = {DayNightCycle.main.timePassedAsFloat}; __instance.timeNextPlay = {__instance.timeNextPlay}");
+				}
+			}
+			catch (Exception ex)
+			{
+				Plugin.Logger.LogError($"An error occurred: {ex}");
+			}
+		}
+	}
+}
